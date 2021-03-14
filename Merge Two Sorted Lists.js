e62bc1b0cec9599fc7d0c7ef89fcccdeff73cea5
nicksolony <nicksolony@gmail.com>
2021-03-13 22:21:48 -0500

    solution works in chrome but doesn't work in leetcode

--- Merge Two Sorted Lists.js
+++ Merge Two Sorted Lists.js
@@ -10,6 +10,52 @@
  * @param {ListNode} l2
  * @return {ListNode}
  */
- var mergeTwoLists = function(l1, l2) {
+
+var mergeTwoLists = function (l1, l2) {
     
+    if((l1.length===0) && (l2.length===0)) {
+        return null;
+    } else if (l1.length===0 && l2.length!==0) {
+        return l2;
+    } else if (l1.length!==0 && l2.length===0) {
+        re;
+    } else {
+      return merge(l1, l2);
+    }
+
+    function merge(l1,l2) {
+        
+        let i1=0;
+        let i2=0;
+        let mergedList = []
+        // debugger
+        while ((i1!==l1.length) || (i2!==l2.length)) {
+            if ((l1[i1]<l2[i2]) || (i2===l2.length)) {
+                mergedList.push(l1[i1])
+                i1++
+                } else {
+                mergedList.push(l2[i2])
+                i2++
+            }
+        }
+        return mergedList;
+        
+    }
+
 };
+
+
+
+
+//this worked in practice, but needed different solution.
+
+// var mergeTwoLists = function(l1, l2) {
+
+//     //merge 2 arrays
+//     let mergedList = l1.concat(l2);
+
+//     //sort arrays
+//     return mergedList.sort();
+
+
+// };
