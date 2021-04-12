c78bfd713f8c1c98c8e5c24277f3410b7d5d39e7
nicksolony <nicksolony@gmail.com>
2021-04-11 21:15:44 -0400

    Remove Duplicates from Sorted Array - completed

--- Remove Duplicates from Sorted Array.js
+++ Remove Duplicates from Sorted Array.js
@@ -2,16 +2,47 @@
  * @param {number[]} nums
  * @return {number}
  */
- var removeDuplicates = function(nums) {
-   if (nums.length===0) {
-       return 0;
-   }
-   let current = 0;
-   for (let i = 0; i < nums.length; i++) {
-       if (nums[current]!==nums[i]) {
-           current++;
-           nums[current]=nums[i]
-       }
-   }
-   return current+1;
- }
+
+
+// THIS SOLITION IS FASTER, BUT IT ONLY GIVES LENGTH OF THE NEW ARRAY WE MIGHT GET, HOWEVER OLD ARRAY REMAINS THE SAME
+//  var removeDuplicates = function(nums) {
+//    if (nums.length===0) {
+//        return 0;
+//    }
+//    let current = 0;
+//    for (let i = 0; i < nums.length; i++) {
+//        if (nums[current]!==nums[i]) {
+//            current++;
+//            nums[current]=nums[i]
+   
+//        }
+   
+//    }
+//        console.log(nums)
+//   return current +1;
+//  }
+
+// THIS SOLUTION I BUILD MYSELF USING RECURSION
+
+var removeDuplicates = function(nums) {
+    if (nums.length===0) {
+        return 0;
+    }
+     
+    dups(0,nums);
+    
+    function dups(current,nums) {
+        let last = nums.length-1;
+        if (current === last) {
+            return nums.length;
+        } else {
+        if (nums[current]!==nums[current+1]) {
+            current++;
+            dups(current,nums);
+        } else {
+            nums.splice(current+1,1);
+            dups(current,nums)
+        }
+        }
+    };
+  };
