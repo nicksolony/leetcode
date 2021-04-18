59ecf67fb16e5ef055679df4d24339bb2fc6e08c
nicksolony <nicksolony@gmail.com>
2021-04-18 18:48:10 -0400

    applied beautify to remove element

--- Remove Element.js
+++ Remove Element.js
@@ -3,19 +3,19 @@
  * @param {number} val
  * @return {number}
  */
- var removeElement = function(nums, val) {
+var removeElement = function (nums, val) {
     if (nums.length === 0) {
         return 0;
     }
-    remove(0, nums,val);
+    remove(0, nums, val);
 
-    function remove(current, nums,val) {
+    function remove(current, nums, val) {
         if (current <= nums.length - 1) {
             if (nums[current] !== val) {
-                remove(current + 1, nums,val);
+                remove(current + 1, nums, val);
             } else {
                 nums.splice(current, 1);
-                remove(current, nums,val)
+                remove(current, nums, val)
             }
         }
     };
