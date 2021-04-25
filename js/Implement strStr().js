eec7ff75fd502cf9f9a28b09e053836e88f983b9
nicksolony <nicksolony@gmail.com>
2021-04-25 13:36:16 -0400

    move JS files to folder

---
+++ js/Implement strStr().js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+ var strStr = function(haystack, needle) {
+
+
+    /*set current position of needle to -1 */
+    let pos = 0;
+    let result = -1;
+    let needleSize = needle.length
+    
+    find(haystack, needle, pos);
+    
+    function find(haystack, needle, pos) {
+        if (pos<=haystack.length-1) {
+            if (haystack.slice(pos,pos+needleSize)===needle) {
+                result = pos;
+            } else {
+                pos++;
+                find(haystack, needle, pos)
+            }    
+        }
+    }
+    
+    return result;
+    
+
+};
