e159a6fa47281406fd94583ebb462610928f44a7
nicksolony <nicksolony@gmail.com>
2021-05-02 17:08:49 -0400

    3. Longest Substring - working solution

--- js/Longest Substring Without Repeating Characters.js
+++ js/Longest Substring Without Repeating Characters.js
@@ -4,27 +4,26 @@
  */
 
 
- var lengthOfLongestSubstring = function(s) {
-   
+var lengthOfLongestSubstring = function (s) {
+
     let longest = 0;
     let substring = []
 
-    for(let e of s) {
-        if(substring.includes(e)) {
+    for (let e of s) {
+        if (substring.includes(e)) {
             if (substring.length > longest) {
                 longest = substring.length
             }
             substring.push(e)
             let index = substring.indexOf(e)
-            substring = substring.slice(index+1)
+            substring = substring.slice(index + 1)
         } else {
             substring.push(e)
         }
     }
 
-    if(substring.length > longest) {
+    if (substring.length > longest) {
         longest = substring.length;
     }
     return longest;
 }
-
