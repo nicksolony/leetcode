2300b83de977a214229052e55419710a297c0f91
nicksolony <nicksolony@gmail.com>
2021-05-02 15:04:05 -0400

    43. Multiply Strings - beautified

--- js/Multiply Strings.js
+++ js/Multiply Strings.js
@@ -3,11 +3,12 @@
  * @param {string} num2
  * @return {string}
  */
- var multiply = function(num1, num2) {
+var multiply = function (num1, num2) {
 
-    let a = num1, b = num2;
+    let a = num1,
+        b = num2;
 
-    if (parseInt(a)==0 || parseInt(b)==0) {
+    if (parseInt(a) == 0 || parseInt(b) == 0) {
         return '0';
     }
 
@@ -15,21 +16,21 @@
     b = b.split('').reverse();
     let result = [];
 
-    for (let i = 0;a[i]>=0; i++) {
-        for (let j = 0; b[j]>=0; j++) {
-            if (!result[i+j]) {
-                result[i+j] = 0;
+    for (let i = 0; a[i] >= 0; i++) {
+        for (let j = 0; b[j] >= 0; j++) {
+            if (!result[i + j]) {
+                result[i + j] = 0;
             }
-            result[i+j] += a[i]*b[j];
-        }        
+            result[i + j] += a[i] * b[j];
+        }
     }
-    for (let i = 0; result[i]>=0; i++ ) {
-        if (result[i]>=10) {
-            if(!result[i+1]){
-                result[i+1]=0;
+    for (let i = 0; result[i] >= 0; i++) {
+        if (result[i] >= 10) {
+            if (!result[i + 1]) {
+                result[i + 1] = 0;
             }
-            result[i+1]+= parseInt(result[i]/10);
-            result[i] %=10;
+            result[i + 1] += parseInt(result[i] / 10);
+            result[i] %= 10;
         }
     }
     return result.reverse().join('');
