f8c7682f14ee3a75c1de61fae7b42595e0203e5a
nicksolony <nicksolony@gmail.com>
2021-05-09 22:31:10 -0400

    53. Maximum Subarray - brut force solution fixed

--- js/Maximum Subarray.js
+++ js/Maximum Subarray.js
@@ -2,18 +2,18 @@
  * @param {number[]} nums
  * @return {number}
  */
- var maxSubArray = function(nums) {
+var maxSubArray = function (nums) {
     // double loop solution
     // starting form first element find max element for each option - this is slow option
-    let maxSum = 0
-    let currSum = 0
-    debugger
+    let currSum = nums[0]
+    let maxSum = currSum;
+
     for (let i = 0; i < nums.length; i++) {
         currSum = nums[i];
         if (currSum > maxSum) {
             maxSum = currSum
         }
-        for (let j = i+1; j < nums.length; j++) {
+        for (let j = i + 1; j < nums.length; j++) {
             currSum = currSum + nums[j];
             if (currSum > maxSum) {
                 maxSum = currSum
