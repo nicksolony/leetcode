6b4b5b8ca95ecbf68f902ac26dfcf7e005a7c8d4
nicksolony <nicksolony@gmail.com>
2021-05-09 22:55:12 -0400

    53. Maximum Subarray - fast solution worked

--- js/Maximum Subarray.js
+++ js/Maximum Subarray.js
@@ -3,30 +3,40 @@
  * @return {number}
  */
 var maxSubArray = function (nums) {
-    // double loop solution
-    // starting form first element find max element for each option - this is slow option
+    
     let currSum = nums[0]
     let maxSum = currSum;
 
-    for (let i = 0; i < nums.length; i++) {
-        currSum = nums[i];
-        if (currSum > maxSum) {
-            maxSum = currSum
-        }
-        for (let j = i + 1; j < nums.length; j++) {
-            currSum = currSum + nums[j];
-            if (currSum > maxSum) {
-                maxSum = currSum
-            }
-        }
-    }
-    return maxSum;
+    // double loop solution
+    // starting form first element find max element for each option - this is slow option
+    // for (let i = 0; i < nums.length; i++) {
+    //     currSum = nums[i];
+    //     if (currSum > maxSum) {
+    //         maxSum = currSum
+    //     }
+    //     for (let j = i + 1; j < nums.length; j++) {
+    //         currSum = currSum + nums[j];
+    //         if (currSum > maxSum) {
+    //             maxSum = currSum
+    //         }
+    //     }
+    // }
+    
 
 
     //Kadane's algorithm solution
     //check subarrays in linear way and reset sum each time current sum is lower than new element of sub array.
+    for (let i = 1; i < nums.length; i++) {
+        if (currSum + nums[i] < nums[i]) {
+            currSum = nums[i]
+        } else {
+            currSum = currSum + nums[i];
+        }
+        if (currSum > maxSum) {
+            maxSum = currSum  
+        }
+    }
 
-
-
+    return maxSum;
 
 };
