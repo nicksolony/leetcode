3d5e4df1dc6ef1b2d3f1d8270258620295320904
nicksolony <nicksolony@gmail.com>
2021-05-13 18:33:08 -0400

    66. Plus One - added notes

--- js/Plus One.js
+++ js/Plus One.js
@@ -3,7 +3,23 @@
  * @return {number[]}
  */
  var plusOne = function(digits) {
-     let lastIndex = digits.length-1 
-  digits[lastIndex]++
-  return digits  
+    // 1. reverse array
+    // 2. add 1, check if < 10
+    // a3. <10 - reverse array back and return
+    // b3. >10 - replace element with result%10
+    // b4. add one to next element and repeat actions
+    
+    let rev = digits.reverse();
+
+    
+
+    function add(arr) {
+
+    }
+    
+    digits[lastIndex]++
+    if (digits[lastIndex]>9 && digits[lastIndex-1]) {
+        
+    }
+    return digits  
 };
