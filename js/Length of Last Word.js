b8c77e80f9436734762727eb3703ba65a6f62fa9
nicksolony <nicksolony@gmail.com>
2021-05-13 18:11:07 -0400

    58. Length of Last Word - trimmed string

--- js/Length of Last Word.js
+++ js/Length of Last Word.js
@@ -3,7 +3,9 @@
  * @return {number}
  */
  var lengthOfLastWord = function(s) {
+    s.trim()
     let arr = s.split(" ");
+    
     if (arr[arr.length-1]) {
         return arr[arr.length-1].length
     } else {
