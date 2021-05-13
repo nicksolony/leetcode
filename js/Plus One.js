b958487f82b71064420f944274338beb85d4cc5f
nicksolony <nicksolony@gmail.com>
2021-05-13 18:43:31 -0400

    66. Plus One - updated to encounter layover

--- js/Plus One.js
+++ js/Plus One.js
@@ -2,24 +2,33 @@
  * @param {number[]} digits
  * @return {number[]}
  */
- var plusOne = function(digits) {
+var plusOne = function (digits) {
     // 1. reverse array
     // 2. add 1, check if < 10
     // a3. <10 - reverse array back and return
     // b3. >10 - replace element with result%10
     // b4. add one to next element and repeat actions
-    
+
     let rev = digits.reverse();
+    let i = 0;
 
-    
-
-    function add(arr) {
-
+    function add(arr, i) {
+        // debugger
+        if (arr[i]) {
+            if (arr[i] + 1 > 9) {
+                arr[i] = (arr[i] + 1) % 10;
+                i++
+                add(arr, i)
+            } else {
+                arr[i] = arr[i] + 1
+            }
+        } else {
+            arr.push(1)
+        }
     }
-    
-    digits[lastIndex]++
-    if (digits[lastIndex]>9 && digits[lastIndex-1]) {
-        
-    }
-    return digits  
+
+    add(rev, i)
+
+    return rev.reverse();
+
 };
