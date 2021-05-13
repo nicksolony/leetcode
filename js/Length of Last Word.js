f372d81d888529e38ff4600b84287801c04e1ea6
nicksolony <nicksolony@gmail.com>
2021-05-13 18:04:26 -0400

    58. Length of Last Word - tesing working solution

--- js/Length of Last Word.js
+++ js/Length of Last Word.js
@@ -3,5 +3,11 @@
  * @return {number}
  */
  var lengthOfLastWord = function(s) {
-    
+    let arr = s.split(" ");
+    if (arr[arr.length-1]) {
+        return arr[arr.length-1].length
+    } else {
+        return 0
+    }
+
 };
