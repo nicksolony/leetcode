60ccd7b15cf07c368b8c4c9f0ce3e681db7716cf
nicksolony <nicksolony@gmail.com>
2021-05-23 17:41:05 -0400

    69. Sqrt(x) - works in chrome, failed in leetcode

--- js/Sqrt(x).js
+++ js/Sqrt(x).js
@@ -3,6 +3,21 @@
  * @return {number}
  */
  var mySqrt = function(x) {
-    return parseInt(Math.sqrt(x));
-    
+    // it workes but not alowed
+    // return parseInt(Math.sqrt(x));
+    if (x===0) {
+        return 0;
+    } else if (x===1) {
+        return 1;
+    } else {
+
+        for (let i = 2; i < x; i++) {
+            if(i*i>x) {
+                let result = i-1;
+                return result;
+            }
+        }
+
+    }
+    return result;
 };
