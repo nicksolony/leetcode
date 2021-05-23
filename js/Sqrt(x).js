e071de5483136798bb428a605294d2565be187e2
nicksolony <nicksolony@gmail.com>
2021-05-23 18:21:54 -0400

    69. Sqrt(x) - copied fastes solution

--- js/Sqrt(x).js
+++ js/Sqrt(x).js
@@ -2,17 +2,38 @@
  * @param {number} x
  * @return {number}
  */
- var mySqrt = function(x) {
+var mySqrt = function (x) {
     // it workes but not alowed
-    // return parseInt(Math.sqrt(x));
-    if (x<=1) {
-        return x;
-    } else {
+    // return Math.floor(Math.sqrt(x));
 
-        for (let i = 2; i <= x; i++) {
-            if(i*i>x) {
-                return i-1;
-            }
+    // workes, but slow
+    // if (x === 0) {
+    //     return 0;
+    // } else if (x === 1) {
+    //     return 1;
+    // } else {
+
+    //     for (let i = 2; i <= x; i++) {
+    //         if (i * i > x) {
+    //             return i - 1;
+    //         }
+    //     }
+    // }
+
+    //copied
+    debugger
+    if(x == 0) {return 0;}
+    let start = 1; let end = Math.ceil(x/2);
+    while(start <= end) {
+        let mid = Math.floor((start + end)/2);
+        if(Math.floor(x/mid) == mid || Math.floor(x/(mid+1)) == mid) { 
+            return mid;
+        } else if(Math.floor(x/mid) > mid) {
+            start = mid+1;
+        } else {
+            end = mid - 1;
         }
     }
+
+
 };
