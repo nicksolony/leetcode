cda5217c7b9127727f8534d959aff9aacbbf4841
nicksolony <nicksolony@gmail.com>
2021-05-26 22:09:35 -0400

    70. Climbing Stairs - working solution

--- js/Climbing Stairs.js
+++ js/Climbing Stairs.js
@@ -3,5 +3,20 @@
  * @return {number}
  */
 var climbStairs = function (n) {
-
+    switch(n) {
+        case 1:
+            return n;
+        case 2:
+            return n;
+        default:
+            let prevStep = 1;
+            let topStep = 2;
+            for (let i = 3; i <= n; i++) {
+                let newBottom=topStep;
+                topStep = topStep + prevStep
+                prevStep = newBottom;
+            };
+            return topStep;
+    }
+    
 };
