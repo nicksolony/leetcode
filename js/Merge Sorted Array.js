51213d74b34c3ab0e67bd47c05adeacb223f7629
nicksolony <nicksolony@gmail.com>
2021-05-30 13:51:32 -0400

    88. Merge Sorted Array - testing 2 (copied and updated)

--- js/Merge Sorted Array.js
+++ js/Merge Sorted Array.js
@@ -5,16 +5,25 @@
  * @param {number} n
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
- var merge = function(nums1, m, nums2, n) {
-    if (m===0 && n>0) {
-        nums1=nums2;
-    } else {
-        for (let i = 0; i < m; i++) {
-            for (let j = 0; j < n; j++) {
-                if(nums1[i]===nums2[j]) {
-                    nums1.splice(i+1,0,nums2[j])
-                }    
-            }            
+var merge = function (nums1, m, nums2, n) {
+
+    let i = m - 1;
+    let j = n - 1;
+    let last = m + n - 1;
+    while (i >= 0 && j >= 0) {
+        if (nums1[i] < nums2[j]) {
+            nums1[last] = nums2[j];
+            last--;
+            j--;
+        } else {
+            nums1[last] = nums1[i];
+            last--;
+            i--;
         }
+    };
+    while (j >= 0) {
+        nums1[last] = nums2[j];
+        last--;
+        j--;
     }
 };
