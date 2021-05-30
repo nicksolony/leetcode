0c9e5d7b15bc54fb973fb54296dbe2180e732606
nicksolony <nicksolony@gmail.com>
2021-05-30 12:27:39 -0400

    83. Remove Duplicates from Sorted List - leetcode working solution

--- js/Remove Duplicates from Sorted List.js
+++ js/Remove Duplicates from Sorted List.js
@@ -9,18 +9,32 @@
  * @param {ListNode} head
  * @return {ListNode}
  */
- var deleteDuplicates = function(head) {
-    dups(0, head);
+// This solution works in the browser, but not in leetcode because it's using an array instead of a list 
+// var deleteDuplicates = function(head) {
+//     dups(0, head);
 
-    function dups(current, nums) {
-        if (current !== nums.length - 1) {  
-            if (nums[current] !== nums[current + 1]) {
-                dups(current + 1, nums);
-            } else {
-                nums.splice(current + 1, 1);
-                dups(current, nums)
-            }
+//     function dups(current, nums) {
+//         if (current !== nums.length - 1) {  
+//             if (nums[current] !== nums[current + 1]) {
+//                 dups(current + 1, nums);
+//             } else {
+//                 nums.splice(current + 1, 1);
+//                 dups(current, nums)
+//             }
+//         }
+//     };
+//     return head;
+// };
+var deleteDuplicates = function(head) {
+    let current = head;
+    while (current && current.next) {
+        if(current.val === current.next.val){
+            current.next = current.next.next;
+        } else {
+            current = current.next;
+            console.log(current)
         }
-    };
+    }
+
     return head;
 };
