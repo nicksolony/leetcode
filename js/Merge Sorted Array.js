cc610bca97fda293f9b45000f29a6bae476ced86
nicksolony <nicksolony@gmail.com>
2021-05-30 13:21:23 -0400

    88. Merge Sorted Array - testing 1 (double loop)

--- js/Merge Sorted Array.js
+++ js/Merge Sorted Array.js
@@ -6,5 +6,15 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
  var merge = function(nums1, m, nums2, n) {
-    
+    if (m===0 && n>0) {
+        nums1=nums2;
+    } else {
+        for (let i = 0; i < m; i++) {
+            for (let j = 0; j < n; j++) {
+                if(nums1[i]===nums2[j]) {
+                    nums1.splice(i+1,0,nums2[j])
+                }    
+            }            
+        }
+    }
 };
