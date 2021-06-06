abf6de77ee76a399731d4ba9fe6b040662696d13
nicksolony <nicksolony@gmail.com>
2021-06-06 19:42:53 -0400

    100. Same Tree - testing code

--- js/Same Tree.js
+++ js/Same Tree.js
@@ -18,4 +18,8 @@ var isSameTree = function (p, q) {
     if (p === null || q === null) {
         return false;
     }
+    if (p.val != q.val) {
+        return false;
+    }
+    return isSameTree(p.right,q.right) && isSameTree(p.left,q.left);
 };
