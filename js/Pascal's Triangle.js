2f3967125ec5058351f417f8e5cdeaf4070f7636
nicksolony <nicksolony@gmail.com>
2021-08-15 21:12:22 -0400

    118. Pascal's Triangle - testing

--- js/Pascal's Triangle.js
+++ js/Pascal's Triangle.js
@@ -4,4 +4,16 @@
  */
  var generate = function(numRows) {
     
+    let values = []
+
+    for (let i = 0; i < numRows; i++) {
+        values[i] = [];
+        values[i][0]=1;
+        for (let j = 1; j < i; j++) {    
+            values[i][j] = values[i-1][j-1]+values[i-1][j]
+        }
+        values[i][i] = 1;
+    }
+
+    return values;
 };
