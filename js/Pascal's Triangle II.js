43feefc58103ffe117b14664a517fad6d12775d1
nicksolony <nicksolony@gmail.com>
2021-08-22 18:45:35 -0400

    119. Pascal's Triangle II  - first test

--- js/Pascal's Triangle II.js
+++ js/Pascal's Triangle II.js
@@ -3,5 +3,17 @@
  * @return {number[]}
  */
  var getRow = function(rowIndex) {
-    
+
+    let values = []
+
+    for (let n = 0; n < rowIndex; n++) {
+        values[n] = [];
+        values[n][0] = 1;
+        for (let k = 1; k < n; k++) {
+            values[n][k] = values[n - 1][k - 1] + values[n - 1][k]
+        }
+        values[n][n] = 1;
+    }
+
+    return values[rowIndex-1];
 };
