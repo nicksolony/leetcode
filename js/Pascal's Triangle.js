52e4bb52257137baf53a084d730740aec0541f0b
nicksolony <nicksolony@gmail.com>
2021-08-22 18:39:30 -0400

    118. Pascal's Triangle - updated formating

--- js/Pascal's Triangle.js
+++ js/Pascal's Triangle.js
@@ -2,17 +2,17 @@
  * @param {number} numRows
  * @return {number[][]}
  */
- var generate = function(numRows) {
-    
+var generate = function (numRows) {
+
     let values = []
 
-    for (let i = 0; i < numRows; i++) {
-        values[i] = [];
-        values[i][0]=1;
-        for (let j = 1; j < i; j++) {    
-            values[i][j] = values[i-1][j-1]+values[i-1][j]
+    for (let n = 0; n < numRows; n++) {
+        values[n] = [];
+        values[n][0] = 1;
+        for (let k = 1; k < n; k++) {
+            values[n][k] = values[n - 1][k - 1] + values[n - 1][k]
         }
-        values[i][i] = 1;
+        values[n][n] = 1;
     }
 
     return values;
