0d5b1151a826897f794fdae5a0187946a5b066e3
nicksolony <nicksolony@gmail.com>
2021-08-29 17:41:37 -0400

    121. Best Time to Buy and Sell Stock - testing one pass solution

--- js/Best Time to Buy and Sell Stock.js
+++ js/Best Time to Buy and Sell Stock.js
@@ -2,26 +2,48 @@
  * @param {number[]} prices
  * @return {number}
  */
+// BRUT FORCE
+//  var maxProfit = function(prices) {
+    
+//     let maxProfit = 0;
+
+//     for (let i = 0; i < prices.length-1; i++) {
+        
+//         let buyPrice = prices[i];
+        
+//         for (let j = i+1; j < prices.length; j++) {
+            
+//             let sellPrice = prices[j];
+
+//             if (sellPrice>buyPrice) {
+//                 let profit = sellPrice - buyPrice;
+//                 // let profit = prices[j]-prices[i];
+//                 if (profit>maxProfit) {
+//                     maxProfit = profit;
+//                 }
+//             }
+
+//         }
+        
+//     }
+
+//     return maxProfit;
+
+// };
+
+
+// ONE PASS
  var maxProfit = function(prices) {
     
     let maxProfit = 0;
-
-    for (let i = 0; i < prices.length-1; i++) {
+    let minPrice = Number.MAX_VALUE;
+    
+    for (let i = 0; i < prices.length; i++) {
         
-        let buyPrice = prices[i];
-        
-        for (let j = i+1; j < prices.length; j++) {
-            
-            let sellPrice = prices[j];
-
-            if (sellPrice>buyPrice) {
-                let profit = sellPrice - buyPrice;
-                // let profit = prices[j]-prices[i];
-                if (profit>maxProfit) {
-                    maxProfit = profit;
-                }
-            }
-
+        if (minPrice>prices[i]) {
+            minPrice = prices[i];
+        } else if (prices[i]-minPrice>maxProfit) {
+            maxProfit = prices[i]-minPrice;
         }
         
     }
