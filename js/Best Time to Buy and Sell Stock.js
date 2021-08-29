d1d3d9001a5afe12fa4955e19fda3c356dd4b9ce
nicksolony <nicksolony@gmail.com>
2021-08-29 16:34:39 -0400

    121. Best Time to Buy and Sell Stock - trying brut force

--- js/Best Time to Buy and Sell Stock.js
+++ js/Best Time to Buy and Sell Stock.js
@@ -4,4 +4,27 @@
  */
  var maxProfit = function(prices) {
     
+    let maxProfit = 0;
+
+    for (let i = 0; i < prices.length-1; i++) {
+        
+        let buyPrice = prices[i];
+        
+        for (let j = i+1; j < prices.length; j++) {
+            
+            let sellPrice = prices[j];
+
+            if (sellPrice>buyPrice) {
+                let profit = sellPrice - buyPrice;
+                if (profit>maxProfit) {
+                    maxProfit = profit;
+                }
+            }
+
+        }
+        
+    }
+
+    return maxProfit;
+
 };
