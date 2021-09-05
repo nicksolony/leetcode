aaa0db5ffdadb3dc4ffadaf31de35c4178f8d9eb
nicksolony <nicksolony@gmail.com>
2021-09-05 16:12:35 -0400

    125. Valid Palindrome - found faster solution

--- js/Valid Palindrome.js
+++ js/Valid Palindrome.js
@@ -6,12 +6,10 @@ var isPalindrome = function (s) {
     let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"" ]/g;
     let cleanString = s.replace(regex, '').toLowerCase();
     let m = parseInt(cleanString.length / 2);
-    let result;
+    let result = true;
 
     for (let i = 0; i <= m; i++) {
-        if (cleanString[i] === cleanString[cleanString.length - 1 - i]) {
-            result = true;
-        } else {
+        if (cleanString[i] != cleanString[cleanString.length - 1 - i]) {
             result = false;
             break;
         }
