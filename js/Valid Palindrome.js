7e10abf7980feca463c6e7425c408c2cf119d245
nicksolony <nicksolony@gmail.com>
2021-09-05 15:39:05 -0400

    125. Valid Palindrome - solved

--- js/Valid Palindrome.js
+++ js/Valid Palindrome.js
@@ -2,14 +2,14 @@
  * @param {string} s
  * @return {boolean}
  */
- var isPalindrome = function(s) {
+var isPalindrome = function (s) {
     let regex = /[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~"" ]/g;
-    let cleanString = s.replace(regex,'').toLowerCase();
-    let m = parseInt(cleanString.length/2);
+    let cleanString = s.replace(regex, '').toLowerCase();
+    let m = parseInt(cleanString.length / 2);
     let result;
 
     for (let i = 0; i <= m; i++) {
-        if (cleanString[i]===cleanString[cleanString.length-1-i]) {
+        if (cleanString[i] === cleanString[cleanString.length - 1 - i]) {
             result = true;
         } else {
             result = false;
