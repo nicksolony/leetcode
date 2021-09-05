aee326ffb52d90be44c5f4aa0261fc3baac1e562
nicksolony <nicksolony@gmail.com>
2021-09-05 15:19:19 -0400

    125. Valid Palindrome -first test

--- js/Valid Palindrome.js
+++ js/Valid Palindrome.js
@@ -3,5 +3,17 @@
  * @return {boolean}
  */
  var isPalindrome = function(s) {
-    
+    let m = parseInt(s.length/2);
+    let result;
+
+    for (let i = 0; i <= m; i++) {
+        
+        if (s[i]===s[s.length-1-i]) {
+            result = true;
+        } else {
+            result = false;
+        }
+    }
+
+    return result;
 };
